a41a86f4afd9ee049541450b7a56a60ae212e792
Debjit Sarkar <sarkarbabudebjit@gmail.com>
2022-12-22 19:19:05 +0530

    Updated addTransaction.dart

--- mini_proj_expense_2/lib/pages/addTransaction.dart
+++ mini_proj_expense_2/lib/pages/addTransaction.dart
@@ -14,29 +14,75 @@ class _AddTransactionState extends State<AddTransaction> {
     return Scaffold(
       appBar: AppBar(toolbarHeight: 0.0),
       body: ListView(
-        padding: const EdgeInsets.all(20),
+        padding: EdgeInsets.all(20),
         children: [
-          const Text("Add Transactions"),
+          Text("Add Transactions",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+          SizedBox(
+            height: 20,
+          ),
           Row(
-            children: const [
-              Icon(Icons.attach_money),
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: Colors.amber),
+                  padding: EdgeInsets.all(12),
+                  child: Icon(
+                    Icons.attach_money,
+                    //color: Colors.white,//change icon color here
+                  )),
+              SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "0",
+                    border: InputBorder.none,
+                  ),
+                  style: TextStyle(fontSize: 24),
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  keyboardType: TextInputType.number,
+                ),
+              ),
             ],
           ),
-          Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "0",
-                border: InputBorder.none,
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: Colors.amber),
+                  padding: EdgeInsets.all(12),
+                  child: Icon(
+                    Icons.description,
+                    //color: Colors.white,//change icon color here
+                  )),
+              SizedBox(
+                width: 12,
               ),
-              style: TextStyle(
-                fontSize: 24,
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Note",
+                    border: InputBorder.none,
+                  ),
+                  style: TextStyle(fontSize: 24),
+                  //inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  //keyboardType: TextInputType.number,
+                  //maxLength: 24,
+                ),
               ),
-              inputFormatters: [
-                FilteringTextInputFormatter.digitsOnly,
-              ],
-              keyboardType: TextInputType.number,
-            ),
-          )
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
         ],
       ),
     );
