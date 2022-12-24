803d783805d3509ed7ab4cf31ebeabf4f1b41ede
Debjit Sarkar <sarkarbabudebjit@gmail.com>
2022-12-24 14:09:45 +0530

    Update homepage.dart

--- mini_proj_expense_2/lib/pages/homepage.dart
+++ mini_proj_expense_2/lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:mini_proj_expense_2/controllers/db_helper.dart';
@@ -67,16 +68,42 @@ class _HomePageState extends State<HomePage> {
                     margin: EdgeInsets.all(12.0),
                     child: Container(
                       decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(24)),
                           gradient: LinearGradient(
                               colors: [Colors.black, Colors.blueAccent])),
-                      child: Column(
-                        children: [
-                          Text(
-                            "Total Balance",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 22, color: Colors.green),
-                          ),
-                        ],
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 8),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Total Balance",
+                              textAlign: TextAlign.center,
+                              style:
+                                  TextStyle(fontSize: 22, color: Colors.white),
+                            ),
+                            SizedBox(height: 12),
+                            Text(
+                              "Rs. 3500",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  fontSize: 26,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                            SizedBox(
+                              height: 12,
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Row(
+                                children: [
+                                  cardIncome("1200"),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -101,6 +128,37 @@ class _HomePageState extends State<HomePage> {
           size: 32.0,
         ),
       ),
+    );
+  }
+
+  Widget cardIncome(String value) {
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          padding: EdgeInsets.all(6),
+          child: Icon(
+            Icons.arrow_downward,
+            size: 28,
+            color: Colors.green,
+          ),
+        ),
+        Column(
+          children: [
+            Text(
+              "Income",
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+            Text(
+              value,
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
